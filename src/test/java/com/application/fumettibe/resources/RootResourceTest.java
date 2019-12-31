19db86cffcf11fe097ee88a3d8b5e723484ad2aa
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-12-31 16:46:53 +0100

    Fixed test

--- src/test/java/com/application/fumettibe/resources/RootResourceTest.java
+++ src/test/java/com/application/fumettibe/resources/RootResourceTest.java
@@ -11,6 +11,7 @@ import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import static org.junit.Assert.*;
 
@@ -25,12 +26,6 @@ public class RootResourceTest {
         server = Main.startServer();
         // create the client
         Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
 
         target = c.target(Main.BASE_URI);
     }
@@ -47,8 +42,7 @@ public class RootResourceTest {
                 .add("op", "ok")
                 .build();
 
-        String responseMsg = target.path("/").request().get(String.class);
-
+        String responseMsg = target.path("/").request(MediaType.APPLICATION_JSON_TYPE).get(String.class);
 
         assertEquals(res.toString(), responseMsg);
     }
