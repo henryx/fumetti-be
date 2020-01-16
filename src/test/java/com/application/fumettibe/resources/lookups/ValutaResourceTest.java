534e214b5cd1a8fbf7c5aca65b25aa5cd59a662e
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-01-17 00:31:32 +0100

    Moved tests in proper package

--- src/test/java/com/application/fumettibe/resources/lookups/ValutaResourceTest.java
+++ src/test/java/com/application/fumettibe/resources/lookups/ValutaResourceTest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-package com.application.fumettibe.resources;
+package com.application.fumettibe.resources.lookups;
 
 import com.application.fumettibe.Main;
 import org.eclipse.jetty.server.Server;
