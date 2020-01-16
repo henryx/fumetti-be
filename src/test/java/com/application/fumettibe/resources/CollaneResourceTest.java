829e17695cd787aea7b9420d9de5f50a76de75a8
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-01-17 00:38:43 +0100

    Added test for collane

---
+++ src/test/java/com/application/fumettibe/resources/CollaneResourceTest.java
@@ -0,0 +1,69 @@
+package com.application.fumettibe.resources;
+
+import com.application.fumettibe.Main;
+import org.eclipse.jetty.server.Server;
+import org.hamcrest.CoreMatchers;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class CollaneResourceTest {
+
+    private Server server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        Main m = new Main();
+
+        // start the server
+        server = m.startServer();
+        server.setStopAtShutdown(true);
+        server.start();
+
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        target = c.target(m.toUrl());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    @Test
+    public void getJson() throws JSONException {
+        Invocation.Builder builder = target.path("/collane").request(MediaType.APPLICATION_JSON_TYPE);
+        var invoke = builder.buildGet().invoke();
+
+        var status = invoke.getStatus();
+
+        Assert.assertThat(Arrays.asList(200, 400), CoreMatchers.hasItem(status));
+
+        if (status == 200) {
+            var responseMsg = invoke.readEntity(ArrayList.class);
+            var responseJson = new JSONArray(responseMsg);
+            Assert.assertThat(responseJson, CoreMatchers.isA(JSONArray.class));
+        } else {
+            var responseMsg = new JSONObject(invoke.readEntity(String.class));
+            Assert.assertTrue(responseMsg.has("op"));
+            Assert.assertTrue(responseMsg.has("motivation"));
+            Assert.assertTrue(responseMsg.has("msg"));
+        }
+    }
+}
