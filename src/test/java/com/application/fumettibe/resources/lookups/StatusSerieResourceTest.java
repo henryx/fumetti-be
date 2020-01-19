6267df22ada71a10df24a38ddf88d3b24bff5892
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-01-19 20:00:58 +0100

    Added status_serie resource

---
+++ src/test/java/com/application/fumettibe/resources/lookups/StatusSerieResourceTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2019 Enrico Bianchi <enrico.bianchi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.application.fumettibe.resources.lookups;
+
+import com.application.fumettibe.Main;
+import org.eclipse.jetty.server.Server;
+import org.hamcrest.CoreMatchers;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+
+public class StatusSerieResourceTest {
+
+    private Server server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        Main m = new Main();
+
+        // start the server
+        server = m.startServer();
+        server.setStopAtShutdown(true);
+        server.start();
+
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        target = c.target(m.toUrl());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    @Test
+    public void getJson() throws JSONException {
+        Invocation.Builder builder = target.path("/status_serie").request(MediaType.APPLICATION_JSON_TYPE);
+        var invoke = builder.buildGet().invoke();
+
+        var status = invoke.getStatus();
+
+        Assert.assertThat(Arrays.asList(200, 400), CoreMatchers.hasItem(status));
+
+        var responseMsg = new JSONObject(invoke.readEntity(String.class));
+        Assert.assertTrue(responseMsg.has("op"));
+
+        if (status == 200) {
+            Assert.assertTrue(responseMsg.has("data"));
+        } else {
+            Assert.assertTrue(responseMsg.has("motivation"));
+            Assert.assertTrue(responseMsg.has("msg"));
+        }
+    }
+}
