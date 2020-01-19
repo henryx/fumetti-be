34567dad898882e0e92c2af76a4079599d968dca
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-01-19 20:05:30 +0100

    Added genere_serie resource

---
+++ src/main/java/com/application/fumettibe/db/resources/lookups/DbGenereSerie.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 Enrico Bianchi <enrico.bianchi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.application.fumettibe.db.resources.lookups;
+
+public class DbGenereSerie extends Lookup {
+    public DbGenereSerie() {
+        super();
+        query = "SELECT id_genere_serie, descrizione FROM genere_serie";
+    }
+}
