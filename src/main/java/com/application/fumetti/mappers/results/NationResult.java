1b3a4f431c7d157783d24d0c0df706b12793349b
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-21 22:51:23 +0200

    Converted to record result mapper

--- src/main/java/com/application/fumetti/mappers/results/NationResult.java
+++ src/main/java/com/application/fumetti/mappers/results/NationResult.java
@@ -3,35 +3,8 @@ package com.application.fumetti.mappers.results;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class NationResult {
-    private final Long id;
-    private final String name;
-    private final String sign;
-    private final CurrencyResult currency;
-
-    public NationResult(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
-                        @JsonProperty("name") String name,
-                        @JsonProperty("sign") String sign,
-                        @JsonProperty("currency") CurrencyResult currency) {
-        this.id = id;
-        this.name = name;
-        this.sign = sign;
-        this.currency = currency;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public CurrencyResult getCurrency() {
-        return currency;
-    }
+public record NationResult(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
+                           @JsonProperty("name") String name,
+                           @JsonProperty("sign") String sign,
+                           @JsonProperty("currency") CurrencyResult currency) {
 }
