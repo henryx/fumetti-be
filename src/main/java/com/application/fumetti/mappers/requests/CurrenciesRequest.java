ad4c585d0b4e5bb9271f79edb9faccbcd9849043
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-21 16:43:28 +0200

    Extended class to manage record

--- src/main/java/com/application/fumetti/mappers/requests/CurrenciesRequest.java
+++ src/main/java/com/application/fumetti/mappers/requests/CurrenciesRequest.java
@@ -1,12 +1,23 @@
 package com.application.fumetti.mappers.requests;
 
 import com.application.fumetti.mappers.results.CurrencyResult;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import java.math.BigDecimal;
 
-public class CurrenciesRequest extends CurrencyResult {
+public class CurrenciesRequest {
+    @JsonUnwrapped
+    private final CurrencyResult data;
 
-    public CurrenciesRequest(String name, String symbol, BigDecimal valueLire, BigDecimal valueEuro) {
-        super(null, name, symbol, valueLire, valueEuro);
+    public CurrenciesRequest(@JsonProperty("name") String name,
+                             @JsonProperty("symbol") String symbol,
+                             @JsonProperty("value_lire") BigDecimal valueLire,
+                             @JsonProperty("value_euro") BigDecimal valueEuro) {
+        this.data = new CurrencyResult(null, name, symbol, valueLire, valueEuro);
+    }
+
+    public CurrencyResult getData() {
+        return data;
     }
 }
