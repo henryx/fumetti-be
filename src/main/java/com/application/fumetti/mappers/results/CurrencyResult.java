3bd645078b4f0a3682ccca3a0b09b142e7bbedb9
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-21 16:42:34 +0200

    Converted to record result mapper

--- src/main/java/com/application/fumetti/mappers/results/CurrencyResult.java
+++ src/main/java/com/application/fumetti/mappers/results/CurrencyResult.java
@@ -5,43 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
 
-public class CurrencyResult {
-    private final Long id;
-    private final String name;
-    private final String symbol;
-    private final BigDecimal valueLire;
-    private final BigDecimal valueEuro;
-
-    public CurrencyResult(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
-                          @JsonProperty("name") String name,
-                          @JsonProperty("symbol") String symbol,
-                          @JsonProperty("value_lire") BigDecimal valueLire,
-                          @JsonProperty("value_euro") BigDecimal valueEuro
-    ) {
-        this.id = id;
-        this.name = name;
-        this.symbol = symbol;
-        this.valueLire = valueLire;
-        this.valueEuro = valueEuro;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public BigDecimal getValueLire() {
-        return valueLire;
-    }
-
-    public BigDecimal getValueEuro() {
-        return valueEuro;
-    }
+public record CurrencyResult(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
+                             @JsonProperty("name") String name,
+                             @JsonProperty("symbol") String symbol,
+                             @JsonProperty("value_lire") BigDecimal valueLire,
+                             @JsonProperty("value_euro") BigDecimal valueEuro) {
 }
