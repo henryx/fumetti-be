764a3eb94bf9d4e7f87fcb6cd4d32ff8aa9135c0
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-23 17:45:14 +0200

    Added entity mapper for editors

---
+++ src/main/java/com/application/fumetti/db/Editors.java
@@ -0,0 +1,27 @@
+package com.application.fumetti.db;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "case_editrici")
+public class Editors extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_casa_editrice", nullable = false)
+    public Long id;
+
+    @Column(name = "nome", length = 30)
+    public String name;
+
+    @Column(name = "sede", length = 30)
+    public String hq;
+
+    @Column(name = "sito_web", length = 30)
+    public String website;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_nazione")
+    public Nations nation;
+}
