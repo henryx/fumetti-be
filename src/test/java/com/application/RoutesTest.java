8ee3d6089cd31d56bbff01c03f0a40404c0f9d61
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-23 17:40:19 +0200

    Added test

--- src/test/java/com/application/RoutesTest.java
+++ src/test/java/com/application/RoutesTest.java
@@ -3,6 +3,7 @@ package com.application;
 import com.application.fumetti.enums.Operations;
 import com.application.fumetti.enums.Results;
 import com.application.fumetti.mappers.Response;
+import com.application.fumetti.mappers.data.EditorData;
 import com.application.fumetti.mappers.requests.CurrenciesRequest;
 import com.application.fumetti.mappers.requests.NationsRequest;
 import com.application.fumetti.mappers.results.CurrencyResult;
@@ -160,6 +161,32 @@ public class RoutesTest {
         }
 
         Assertions.assertEquals(res.getOperation(), Operations.LOOKUP.getOperation());
+        Assertions.assertEquals(Results.OK.getResult(), res.getResult());
+    }
+
+    @Test
+    @Order(3)
+    public void postEditors() throws JsonProcessingException {
+        final String BASE_PATH = "/editors";
+
+        var req = new EditorData(null, "test editore", "test sede", "https://sito",
+                new NationResult(1L, "Italia", "IT",
+                        new CurrencyResult(1L, "Euro", "€", new BigDecimal("1936.27"),
+                                new BigDecimal("1.00"))));
+
+        var json = this.mapper.writeValueAsString(req);
+        var resp = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(json)
+                .post(BASE_PATH);
+
+        resp.then().assertThat().statusCode(200);
+
+        var body = resp.body().asString();
+        var res = this.mapper.readValue(body, Response.class);
+
+        Assertions.assertEquals(Operations.EDITORS.getOperation(), res.getOperation());
         Assertions.assertEquals(Results.OK.getResult(), res.getResult());
     }
 }
