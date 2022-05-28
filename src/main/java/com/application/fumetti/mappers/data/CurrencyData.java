ae251fb0ea559ee6f4ef8227beffa8ba770f0603
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-28 22:03:09 +0200

    Added static mapper for currency record

--- src/main/java/com/application/fumetti/mappers/data/CurrencyData.java
+++ src/main/java/com/application/fumetti/mappers/data/CurrencyData.java
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 public record CurrencyData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
                            @JsonProperty("name") String name,
                            @JsonProperty("symbol") String symbol,
                            @JsonProperty("value_lire") BigDecimal valueLire,
                            @JsonProperty("value_euro") BigDecimal valueEuro) {
+
+    public static CurrencyData map(HashMap<String, Object> data) {
+        return new CurrencyData(Long.getLong(data.get("id").toString()), data.get("name").toString(),
+                data.get("symbol").toString(), new BigDecimal(data.get("value_lire").toString()),
+                new BigDecimal(data.get("value_euro").toString()));
+    }
 }
