8b042341ebba667a2cd1b10c119e6c95fc96baa1
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-05-28 22:07:25 +0200

    Added static mapper for nation record

--- src/main/java/com/application/fumetti/mappers/data/NationData.java
+++ src/main/java/com/application/fumetti/mappers/data/NationData.java
@@ -3,8 +3,18 @@ package com.application.fumetti.mappers.data;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+
 public record NationData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
                          @JsonProperty("name") String name,
                          @JsonProperty("sign") String sign,
                          @JsonProperty("currency") CurrencyData currency) {
+
+    public static NationData map(HashMap<String, Object> data) {
+        var nestedMap = (HashMap<String, Object>) data.get("currency");
+        var currency = CurrencyData.map(nestedMap);
+
+        return new NationData(Long.getLong(data.get("id").toString()), data.get("name").toString(),
+                data.get("sign").toString(), currency);
+    }
 }
