abc37407f441d9dd8e06b48e3ceee40a1856369b
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-06-29 17:45:47 +0200

    Added database entity mapper for collections

---
+++ src/main/java/com/application/fumetti/db/Collections.java
@@ -0,0 +1,22 @@
+package com.application.fumetti.db;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "collane", schema = "public")
+public class Collections extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_collana", nullable = false)
+    public Long id;
+
+    @Column(name = "nome", length = 30)
+    public String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_casa_editrice")
+    public Editors editor;
+}
+
