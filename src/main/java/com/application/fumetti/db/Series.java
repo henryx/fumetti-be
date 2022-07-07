86c595742587f70638603e24a3a21540cf7e6223
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-07 23:58:42 +0200

    Added entity used to manage series data

---
+++ src/main/java/com/application/fumetti/db/Series.java
@@ -0,0 +1,34 @@
+package com.application.fumetti.db;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "serie")
+public class Series {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_serie", nullable = false)
+    public Long id;
+
+    @Column(name = "nome", length = 30)
+    public String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_collana")
+    public Collections collection;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_status_serie")
+    public SeriesStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_periodicita")
+    public SeriesFrequency frequency;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_genere_serie")
+    public SeriesGenre genre;
+
+    @Column(name = "note", columnDefinition="TEXT")
+    public String note;
+}
