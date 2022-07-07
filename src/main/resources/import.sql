efb0bd97e45cbe43d1c4a40de31fb6ea29fbd721
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-07 23:51:53 +0200

    Added SQL script for first creation imports

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO genere_serie VALUES (1, 'Horror');
+INSERT INTO genere_serie VALUES (2, 'Combattimento');
+INSERT INTO genere_serie VALUES (3, 'Sentimentale');
+INSERT INTO genere_serie VALUES (4, 'Comico');
+INSERT INTO genere_serie VALUES (5, 'Avventura');
+INSERT INTO genere_serie VALUES (6, 'Sportivo');
+
+INSERT INTO periodicita VALUES (1, 'Settimanale');
+INSERT INTO periodicita VALUES (2, 'Quindicinale');
+INSERT INTO periodicita VALUES (3, 'Mensile');
+INSERT INTO periodicita VALUES (4, 'Bimestrale');
+INSERT INTO periodicita VALUES (98, 'Aperiodico');
+INSERT INTO periodicita VALUES (99, 'Volume unico');
+
+INSERT INTO status_serie VALUES (1, 'In corso');
+INSERT INTO status_serie VALUES (2, 'Terminata');
+INSERT INTO status_serie VALUES (3, 'Interrotta');
