ee6fe8b4b628b37b4dfc09efaf5d8d5ad3432ae5
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-19 19:57:19 +0200

    Implemented POST method in series resource

--- src/main/java/com/application/fumetti/resources/Series.java
+++ src/main/java/com/application/fumetti/resources/Series.java
@@ -1,5 +1,11 @@
 package com.application.fumetti.resources;
 
+import com.application.fumetti.db.Collections;
+import com.application.fumetti.db.SeriesFrequency;
+import com.application.fumetti.db.SeriesGenre;
+import com.application.fumetti.db.SeriesStatus;
+import com.application.fumetti.enums.Operations;
+import com.application.fumetti.enums.Results;
 import com.application.fumetti.mappers.Response;
 import com.application.fumetti.mappers.data.SeriesData;
 
@@ -18,6 +24,21 @@ public class Series {
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     public Response<SeriesData> postSeries(SeriesData req) {
-        return null;
+        Collections collection = Collections.findById(req.collection().id());
+        SeriesFrequency frequency = SeriesFrequency.find("description", req.frequency()).firstResult();
+        SeriesStatus status = SeriesStatus.find("description", req.status()).firstResult();
+        SeriesGenre genre = SeriesGenre.find("description", req.genre()).firstResult();
+
+        var series = new com.application.fumetti.db.Series();
+        series.name = req.name();
+        series.note = req.note();
+
+        series.collection = collection;
+        series.frequency = frequency;
+        series.status = status;
+        series.genre = genre;
+        series.persist();
+
+        return new Response<>(Operations.SERIES, Results.OK);
     }
 }
