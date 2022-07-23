002769df1bbbcf586f1e2b14dec0e26b09c8f90d
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-23 12:46:37 +0200

    Added entity lookup for bindings

---
+++ src/main/java/com/application/fumetti/db/lookup/books/Bindings.java
@@ -0,0 +1,17 @@
+package com.application.fumetti.db.lookup.books;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rilegatura", schema = "public", catalog = "fumetti")
+public class Bindings extends PanacheEntityBase {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id_rilegatura")
+    public Long id;
+
+    @Column(name = "descrizione")
+    public String description;
+}
