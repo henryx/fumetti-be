411524e30a6a5ed784dc3826ce7b3083a8218b39
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-25 23:48:19 +0200

    Implemented GET method used for return book's preservations data

--- src/main/java/com/application/fumetti/resources/Books.java
+++ src/main/java/com/application/fumetti/resources/Books.java
@@ -1,10 +1,12 @@
 package com.application.fumetti.resources;
 
 import com.application.fumetti.db.lookup.books.Bindings;
+import com.application.fumetti.db.lookup.books.Preservations;
 import com.application.fumetti.enums.Operations;
 import com.application.fumetti.enums.Results;
 import com.application.fumetti.mappers.Response;
 import com.application.fumetti.mappers.data.lookup.books.BindingsData;
+import com.application.fumetti.mappers.data.lookup.books.PreservationsData;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,6 +22,17 @@ public class Books {
     public Response<BindingsData> getBooksBindings() {
         var data = Bindings.findAll().stream().map(e -> BindingsData.map((Bindings) e)).toList();
         var resp = new Response<BindingsData>(Operations.LOOKUP, Results.OK);
+        resp.setData(data);
+
+        return resp;
+    }
+
+    @GET
+    @Path("/preservations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response<PreservationsData> getBooksPreservations() {
+        var data = Preservations.findAll().stream().map(e -> PreservationsData.map((Preservations) e)).toList();
+        var resp = new Response<PreservationsData>(Operations.LOOKUP, Results.OK);
         resp.setData(data);
 
         return resp;
