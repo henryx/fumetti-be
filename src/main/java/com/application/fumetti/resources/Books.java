2625c11ea2ed263105483c9556eb55ce38559056
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-07-29 23:01:20 +0200

    Written POST method used for save books

--- src/main/java/com/application/fumetti/resources/Books.java
+++ src/main/java/com/application/fumetti/resources/Books.java
@@ -1,20 +1,48 @@
 package com.application.fumetti.resources;
 
+import com.application.fumetti.db.Currencies;
+import com.application.fumetti.db.Series;
 import com.application.fumetti.db.lookup.books.Bindings;
 import com.application.fumetti.db.lookup.books.Preservations;
 import com.application.fumetti.enums.Operations;
 import com.application.fumetti.enums.Results;
 import com.application.fumetti.mappers.Response;
+import com.application.fumetti.mappers.data.BooksData;
 import com.application.fumetti.mappers.data.lookup.books.BindingsData;
 import com.application.fumetti.mappers.data.lookup.books.PreservationsData;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Path("/books")
 public class Books {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response<BooksData> postCollections(BooksData req) {
+        Series series = Series.findById(req.series().id());
+        Currencies currency = Currencies.findById(req.currency().id());
+        Bindings binding = Bindings.findById(req.binding().id());
+        Preservations preservation = Preservations.findById(req.preservation().id());
+
+        var book = new com.application.fumetti.db.Books();
+        book.series = series;
+        book.number = req.number();
+        book.published = LocalDate.parse(req.published(), DateTimeFormatter.ISO_LOCAL_DATE);
+        book.price = req.price();
+        book.currency = currency;
+        book.binding = binding;
+        book.preservation = preservation;
+        book.note = req.note();
+        book.persist();
+
+        return new Response<>(Operations.BOOKS, Results.OK);
+    }
 
     @GET
     @Path("/bindings")
