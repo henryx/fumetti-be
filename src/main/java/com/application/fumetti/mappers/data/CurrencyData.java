3ccac38ee56af00e7ed4b3c343c8c92396203dec
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-08-17 19:34:01 +0200

    Added default data for records

--- src/main/java/com/application/fumetti/mappers/data/CurrencyData.java
+++ src/main/java/com/application/fumetti/mappers/data/CurrencyData.java
@@ -10,13 +10,14 @@ import java.util.HashMap;
 public record CurrencyData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
                            @JsonProperty("name") String name,
                            @JsonProperty("symbol") String symbol,
-                           @JsonProperty("value_lire") BigDecimal valueLire,
-                           @JsonProperty("value_euro") BigDecimal valueEuro) {
+                           @JsonProperty("value_lire") @JsonInclude(JsonInclude.Include.NON_NULL) BigDecimal valueLire,
+                           @JsonProperty("value_euro") @JsonInclude(JsonInclude.Include.NON_NULL) BigDecimal valueEuro) {
 
     public static CurrencyData map(HashMap<String, Object> data) {
         return new CurrencyData(Long.valueOf(data.get("id").toString()), data.get("name").toString(),
-                data.get("symbol").toString(), new BigDecimal(data.get("value_lire").toString()),
-                new BigDecimal(data.get("value_euro").toString()));
+                data.get("symbol").toString(),
+                new BigDecimal(data.getOrDefault("value_lire", "0").toString()),
+                new BigDecimal(data.getOrDefault("value_euro", "0").toString()));
     }
 
     public static CurrencyData map(Currencies map) {
--- src/main/java/com/application/fumetti/mappers/data/EditorData.java
+++ src/main/java/com/application/fumetti/mappers/data/EditorData.java
@@ -8,8 +8,8 @@ import java.util.HashMap;
 
 public record EditorData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
                          @JsonProperty("name") String name,
-                         @JsonProperty("hq") String hq,
-                         @JsonProperty("website") String website,
+                         @JsonProperty("hq") @JsonInclude(JsonInclude.Include.NON_NULL) String hq,
+                         @JsonProperty("website") @JsonInclude(JsonInclude.Include.NON_NULL) String website,
                          @JsonProperty("nation") NationData nation) {
 
     public static EditorData map(HashMap<String, Object> data) {
@@ -17,7 +17,8 @@ public record EditorData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NO
         var nation = NationData.map(nestedMap);
 
         return new EditorData(Long.valueOf(data.get("id").toString()), data.get("name").toString(),
-                data.get("hq").toString(), data.get("website").toString(), nation);
+                data.getOrDefault("hq", "").toString(),
+                data.getOrDefault("website", "").toString(), nation);
     }
 
     public static EditorData map(Editors map) {
