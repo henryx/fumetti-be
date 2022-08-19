3bd13bcc9e3ef4d45655772efa4d277af82c7a4b
Enrico Bianchi <enrico.bianchi@gmail.com>
2022-08-19 17:18:14 +0200

    Modified record using new references for series data

--- src/main/java/com/application/fumetti/mappers/data/SeriesData.java
+++ src/main/java/com/application/fumetti/mappers/data/SeriesData.java
@@ -11,36 +11,35 @@ import java.util.HashMap;
 
 public record SeriesData(@JsonProperty("id") @JsonInclude(JsonInclude.Include.NON_NULL) Long id,
                          @JsonProperty("name") String name,
-                         @JsonProperty("collection") CollectionData collection,
                          @JsonProperty("genre") GenreData genre,
+                         @JsonProperty("editor") EditorData editor,
                          @JsonProperty("frequency") FrequencyData frequency,
                          @JsonProperty("status") StatusData status,
                          @JsonProperty("note") String note
 ) {
 
     public static SeriesData map(HashMap<String, Object> data) {
-        @SuppressWarnings("unchecked") var nestedCollection = (HashMap<String, Object>) data.get("collection");
         @SuppressWarnings("unchecked") var nestedGenre = (HashMap<String, Object>) data.get("genre");
         @SuppressWarnings("unchecked") var nestedFrequency = (HashMap<String, Object>) data.get("frequency");
         @SuppressWarnings("unchecked") var nestedStatus = (HashMap<String, Object>) data.get("status");
+        @SuppressWarnings("unchecked") var nestedEditor = (HashMap<String, Object>) data.get("editor");
 
 
-        var collection = CollectionData.map(nestedCollection);
         var genre = GenreData.map(nestedGenre);
         var frequency = FrequencyData.map(nestedFrequency);
         var status = StatusData.map(nestedStatus);
+        var editor = EditorData.map(nestedEditor);
 
         return new SeriesData(Long.valueOf(data.get("id").toString()), data.get("name").toString(),
-                collection, genre, frequency, status, data.get("note").toString());
+                genre, editor, frequency, status, data.get("note").toString());
     }
 
     public static SeriesData map(Series map) {
-        var collection = CollectionData.map(map.collection);
         var genre = GenreData.map(map.genre);
         var frequency = FrequencyData.map(map.frequency);
         var status = StatusData.map(map.status);
+        var editor = EditorData.map(map.editor);
 
-        return new SeriesData(map.id, map.name, collection, genre, frequency,
-                status, map.note);
+        return new SeriesData(map.id, map.name, genre, editor, frequency, status, map.note);
     }
 }
